e1febada388a81aea8357c9e7e25fb20266bc85a
rebeka.pilsikova <rebeka.pilscikova@visma.com>
2025-03-18 09:23:20 +0200

    18.03

---
+++ PicasPiegadesSistema/Hashing.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Reflection.Metadata.Ecma335;
+using System.DirectoryServices;
+
+namespace PicasPiegadesSistema
+{
+    class Hashing
+    {
+        public static string GeneratePasswordHash(string password)
+        {
+            byte[] bytes = Encoding.Unicode.GetBytes(password);
+            byte[] hashedBytes = HashAlgorithm.Create("SHA1").ComputeHash(bytes);
+
+            return Convert.ToBase64String(hashedBytes);
+        }
+
+        public static bool CheckPasswordHash(string password, string hash)
+        {
+            string passwordHash = GeneratePasswordHash(password);
+            return passwordHash.Equals(hash);
+        }
+    }
+}
