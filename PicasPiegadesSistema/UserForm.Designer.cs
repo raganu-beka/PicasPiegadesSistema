c5c2cd45fffac9fb87cc2bceb7cccedb5c77a1a4
rebeka.pilsikova <rebeka.pilscikova@visma.com>
2025-03-24 09:27:01 +0200

    24.03

--- PicasPiegadesSistema/UserForm.Designer.cs
+++ PicasPiegadesSistema/UserForm.Designer.cs
@@ -28,12 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "UserForm";
+            unameTxt = new TextBox();
+            passTxt = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            loginBtn = new Button();
+            SuspendLayout();
+            // 
+            // unameTxt
+            // 
+            unameTxt.Location = new Point(45, 58);
+            unameTxt.Name = "unameTxt";
+            unameTxt.Size = new Size(296, 23);
+            unameTxt.TabIndex = 0;
+            // 
+            // passTxt
+            // 
+            passTxt.Location = new Point(45, 113);
+            passTxt.Name = "passTxt";
+            passTxt.Size = new Size(296, 23);
+            passTxt.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(45, 40);
+            label1.Name = "label1";
+            label1.Size = new Size(74, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Lietotājvārds";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(45, 95);
+            label2.Name = "label2";
+            label2.Size = new Size(40, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Parole";
+            // 
+            // loginBtn
+            // 
+            loginBtn.Location = new Point(44, 154);
+            loginBtn.Name = "loginBtn";
+            loginBtn.Size = new Size(75, 23);
+            loginBtn.TabIndex = 4;
+            loginBtn.Text = "Pieslēgties!";
+            loginBtn.UseVisualStyleBackColor = true;
+            // 
+            // UserForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(391, 220);
+            Controls.Add(loginBtn);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(passTxt);
+            Controls.Add(unameTxt);
+            Name = "UserForm";
+            Text = "UserForm";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox unameTxt;
+        private TextBox passTxt;
+        private Label label1;
+        private Label label2;
+        private Button loginBtn;
     }
 }
--- PicasPiegadesSistema/UserForm.cs
+++ PicasPiegadesSistema/UserForm.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace PicasPiegadesSistema
 {
-    public partial class UserForm: Form
+    public partial class UserForm : Form
     {
         public UserForm()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
